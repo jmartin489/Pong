d177ac9fd5727bad1686e35a3b38295fa9b59559
Victoria Frantsuh <eboko1@gmail.com>
2017-01-02 23:06:26 +0200

    move Ball

--- src/Ball.java
+++ src/Ball.java
@@ -1,25 +1,28 @@
 import java.awt.*;
-import java.util.Random;
 
 /**
  * Created by Vika on 31.12.2016.
  */
 public class Ball {
     private int x=250;
-    private int y=0;
+    private int y=250;
 
-    private int xVelocity = 0; // move
-    private int yVelocity = -1;
+    private int xVelocity = -1; // move
+    private int yVelocity = 0;
 
     public void update(){
         x = x + xVelocity;
         y = y - yVelocity;// - ball down   + ball up
+
+        if (x<0  ){
+            xVelocity=1;
+        }
     }
     public void paint(Graphics g){
         g.setColor(Color.blue);
         g.fillOval(x,y,30,30);
         g.setColor(Color.RED);
-        g.fillRect(x+40,y+40,20,20);}
+        g.fillRect(x+40,y+40,20,20);
 
 
     }
