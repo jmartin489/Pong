15d9d1eff2f1bc159ae98756c15bcea85ccf25b9
Victoria Frantsuh <eboko1@gmail.com>
2017-01-04 23:15:20 +0200

    adding size

--- src/Ball.java
+++ src/Ball.java
@@ -9,18 +9,21 @@ public class Ball {
 
     private int xVelocity = -1; // move
     private int yVelocity = 0;
+    private int size =30;
 
     public void update(){
         x = x + xVelocity;
-        y = y - yVelocity;// - ball down   + ball up
+        y = y + yVelocity;// - ball down   + ball up
 
         if (x<0  ){
             xVelocity=1;
-        }
-    }
+        } else if(x>500-size){
+            xVelocity = -1;
+
+        } }
     public void paint(Graphics g){
         g.setColor(Color.blue);
-        g.fillOval(x,y,30,30);
+        g.fillOval(x,y,size,size);
         g.setColor(Color.RED);
         g.fillRect(x+40,y+40,20,20);
 
